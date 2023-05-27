64e7ed05218ae69324515648db3b7756bda80680
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-05-27 09:03:13 +0300

    fix bug

--- proxy.py
+++ proxy.py
@@ -2,7 +2,9 @@ import sys
 import socket
 import threading
 
-HEX_FILTER = ''.join([len(repr(chr(i))) == 3 ) and chr(i) or '.' for i in range(256)])
+HEX_FILTER = ''.join(
+        [(len(repr(chr(i))) == 3 ) and chr(i) or '.' for i in range(256)]
+        )
 
 def hexdump(src, length=16, show=True):
     if isinstance(src, bytes):
